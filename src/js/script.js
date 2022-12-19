caa418370a1230bf58c6ef2692e09b0f01b5b191
ostapvasilenko <ostapvasilenko55@gmail.com>
2022-12-19 11:32:46 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,13 @@
 //     infinite: true
 //   });
 // });
+
+$('.blog__tabs_tab').on('click', function() {
+    $('.blog__tabs_tab').removeClass('active');
+    $(this).addClass('active');
+    let tabs = $(this).data("tabs");
+
+    $('.blog__card').addClass('d-none');
+    $('.blog__card[data-tabs =' + tabs +']').removeClass('d-none');
+    $('.blog__card[data-tabs =' + tabs +']').addClass('d-block');
+})
